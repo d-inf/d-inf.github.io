f927632ebed0e68e4f33bf67816a246a30ff7d86
d-inf <142904010+d-inf@users.noreply.github.com>
2023-08-22 19:57:25 +0300

    Update script.js
    number added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.querySelector('.whatsapp-button').addEventListener('click', function () {
     // Replace 'insert-number-here' with the actual WhatsApp number you want to use
-    window.open(`https://wa.me/insert-number-here`, '_blank');
+    window.open(`https://wa.me/+447909636114`, '_blank');
 });
