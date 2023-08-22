c3e29e49a8497dac976b29a0897c3fdfd3226f28
d-inf <142904010+d-inf@users.noreply.github.com>
2023-08-22 16:10:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.whatsapp-button').addEventListener('click', function () {
+    // Replace 'insert-number-here' with the actual WhatsApp number you want to use
+    window.open(`https://wa.me/insert-number-here`, '_blank');
+});
